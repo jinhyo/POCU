03d1db2d015f29c1437faf2c83b4d3c22d371524
sosilion <sosilion@gmail.com>
2019-09-16 00:48:47 +0900

    Lab2(try 1)

---
+++ Lab2/Labs2.cpp
@@ -0,0 +1,55 @@
+#include "Lab2.h"
+#include <iomanip>
+
+using namespace std;
+namespace lab2
+{
+	void PrintIntegers(std::istream& in, std::ostream& out)
+	{
+		int num;
+		string trash;
+		int column = 12;
+		out << right;
+		out << setw(column) <<setfill(' ')<< "oct" << setw(column) << "dec" << setw(column) << "hex" << endl<<setfill('-');
+		out << setw(column) << ' ' << setw(column) << ' ' << setw(column) << ' ' << endl<<setfill(' ');
+		
+		while (!in.eof())
+		{
+			in >> num;
+			if (in.fail())
+			{
+				in.clear();
+				in >> trash;
+			}
+			else
+				out << setw(column) << oct << num << setw(column) << dec << num << setw(column) << uppercase << hex << num << endl;
+		}
+		in.clear();
+	}
+
+	void PrintMaxFloat(std::istream& in, std::ostream& out)
+	{
+		float num, max=0;
+		string trash;
+		int leftColumn = 5;
+		int rightColumn= 15;
+		out << right<<fixed<<setprecision(3)<<showpos<<internal;
+
+		while (!in.eof())
+		{
+			in >> num;
+			num > max ? max = num : max = max;
+			if (in.fail())
+			{
+				in.clear();
+				in >> trash;
+			}
+			else
+				out << setw(leftColumn) << setfill(' ') << "" <<
+				setw(rightColumn) << num << endl;
+		}
+		in.clear();
+		out << setw(leftColumn) << setfill(' ')<<"max: " <<
+			setw(rightColumn) << max << endl;
+	}
+}
--- Lab2/main.cpp
+++ Lab2/main.cpp
@@ -1,4 +1,11 @@
+#include "Lab2.h"
+#include <sstream>
+
+using namespace std;
 int main()
 {
-
+	lab2::PrintIntegers(cin, cout);
+	lab2::PrintMaxFloat(cin, cout);
+	system("pause");
+	return 0;
 }
