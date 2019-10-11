de55282333ca50bc85aaee03f555cb05d27a4003
sosilion <sosilion@gmail.com>
2019-10-11 16:03:29 +0900

    2차

--- Lab6/Lab6.cpp
+++ Lab6/Lab6.cpp
@@ -4,7 +4,7 @@ namespace lab6
 {
 	int Sum(const std::vector<int>& v)
 	{
-		int total=0;
+		int total = 0;
 		if (v.size() < 1)
 		{
 			return 0;
@@ -78,12 +78,17 @@ namespace lab6
 
 	int NumberWithMaxOccurrence(const std::vector<int>& v)
 	{
+		if (v.size() < 1)
+		{
+			return 0;
+		}
+
 		int majorityCount = 0;
 		int majority = 0;
-		for (size_t i = 0; i < v.size()-1; i++)
+		for (size_t i = 0; i < v.size() - 1; i++)
 		{
 			int count = 0;
-			for (size_t j = i+1; j < v.size(); j++)
+			for (size_t j = i + 1; j < v.size(); j++)
 			{
 				if (v[i] == v[j])
 				{
@@ -101,9 +106,9 @@ namespace lab6
 
 	void SortDescending(std::vector<int>& v)
 	{
-		for (size_t i = 0; i < v.size()-1; i++)
+		for (size_t i = 0; i < v.size() - 1; i++)
 		{
-			for (size_t j = i+1; j < v.size(); j++)
+			for (size_t j = i + 1; j < v.size(); j++)
 			{
 				int temp;
 				if (v[i] - v[j] < 0)
